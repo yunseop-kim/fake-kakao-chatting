55d7be44edc805bc7d6b93ce8463527d2a94fe73
Yunseop Kim <undefined@mealant.com>
2019-05-29 18:57:21 +0900

    광고 배너 변경

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,27 +3,23 @@
     <div class="modal-mask">
       <div class="modal-wrapper">
         <div class="modal-container">
-
           <div class="modal-header">
-            <slot name="header">
-            </slot>
+            <slot name="header"></slot>
           </div>
 
           <div class="modal-body">
-            <slot name="body">
-              default body
-            </slot>
+            <slot name="body">default body</slot>
           </div>
 
           <div class="modal-footer">
             <slot name="footer">
-              <button class="modal-default-button" @click="$emit('close')">
-                OK
-              </button>
+              <button class="modal-default-button" @click="$emit('close')">OK</button>
             </slot>
             <div>
               <span style="font-size: 10px;">아래 광고를 눌러주시면 운영에 큰 힘이 됩니다. (_ _)</span>
-              <Adsense data-ad-client="ca-pub-4995410357973680" data-ad-slot="3055391333" />
+              <a :href="ad.href" target="_blank">
+                <img :src="ad.src">
+              </a>
             </div>
           </div>
         </div>
@@ -34,8 +30,85 @@
 
 <script>
 export default {
-  name: 'Modal'
-}
+  name: "Modal",
+  computed: {
+    ad() {
+      const random = Math.floor(Math.random() * this.ads.length - 1);
+      return this.ads[random];
+    }
+  },
+  data() {
+    return {
+      ads: [
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/coupang0516-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtRg"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/gold-box-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtRP"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/rocket-w0218-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtR2"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/sns-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtSg"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/jikgu1114-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtSs"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/fashion-women-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtSK"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/fashion-men-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtTj"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/beauty-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtTG"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/food-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtTW"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/kitchen-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtUb"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/electronic-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtUK"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/sports-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtV0"
+        },
+        {
+          src:
+            "https://static.coupangcdn.com/image/affiliate/category/20180610/toy-150-x-60.jpg",
+          href: "https://coupa.ng/bhgtWl"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -47,9 +120,9 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, .5);
+  background-color: rgba(0, 0, 0, 0.5);
   display: table;
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .modal-wrapper {
@@ -63,8 +136,8 @@ export default {
   padding: 20px 30px;
   background-color: #fff;
   border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-  transition: all .3s ease;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
   font-family: Helvetica, Arial, sans-serif;
 }
 
